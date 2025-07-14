ea88946357e1ce2028b5595a5ec788295a504b85
Shivani Sharma <shivani@example.com>
2025-07-14 19:16:58 +0530

    PyQt5 digital clock

---
+++ README.md
@@ -0,0 +1,47 @@
+# 🕒 Digital Clock using PyQt5
+
+A sleek and minimal **digital clock GUI application** built using Python and PyQt5.  
+It displays the current system time with custom digital-style fonts and real-time updates every second.
+
+---
+
+## 🚀 Features
+
+- Custom digital font (DS_DIGIT.TTF)
+- Real-time clock updates every second
+- Styled with green-on-black digital theme
+- Center-aligned time display
+- Clean and responsive layout using `QVBoxLayout`
+
+---
+
+## 📸 Screenshot
+
+![Digital Clock Screenshot](screenshot.png)
+
+
+---
+
+## 🛠️ Technologies Used
+
+- Python 3.x
+- PyQt5
+- QTimer, QLabel, QVBoxLayout
+- QFontDatabase for custom font handling
+
+---
+
+## 📦 How to Run
+
+Make sure PyQt5 is installed:
+```bash
+pip install PyQt5
+
+Then, run the program:
+python digitalClock.py
+
+Ensure the following files are in the same directory:
+
+digitalClock.py
+DS_DIGIT.TTF
+screenshot.png (optional, for preview)
---
+++ main.py
@@ -0,0 +1,48 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget , QLabel , QVBoxLayout
+from PyQt5.QtCore import QTimer , QTime , Qt
+from PyQt5.QtGui import QFont , QFontDatabase
+
+class digitalClock(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.time_label = QLabel(self)
+    self.timer = QTimer(self)
+    self.initUI()
+
+
+  def initUI(self):
+
+    self.setWindowTitle("Digital Clock")
+    self.setGeometry(600,400,300,100)
+    self.time_label.setAlignment(Qt.AlignCenter)
+    self.time_label.setStyleSheet("font-size: 150px;"
+                                  "color: hsl(126, 90%, 61%);")
+    self.setStyleSheet("background-color: black;")
+
+    vbox = QVBoxLayout()
+    vbox.addWidget(self.time_label)
+    self.setLayout(vbox)
+
+    self.timer.timeout.connect(self.update_time)
+    self.timer.start(1000)
+
+    font_id = QFontDatabase.addApplicationFont("DS-DIGIT.TTF")
+    font_family= QFontDatabase.applicationFontFamilies(font_id)[0]
+    myfont = QFont(font_family , 150)
+    self.time_label.setFont(myfont)
+
+    self.update_time()
+
+  def update_time(self):
+    current_time = QTime.currentTime().toString("hh:mm:ss AP")
+    self.time_label.setText(current_time)
+
+    
+
+if __name__== "__main__":
+  app = QApplication(sys.argv)
+  clock = digitalClock()
+  clock.show()
+  sys.exit(app.exec_())
+
